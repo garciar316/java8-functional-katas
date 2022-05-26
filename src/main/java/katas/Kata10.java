b5c384c9afe176fc8e33dcd8e8e427c72396a3af
garciar316 <sergio980316@hotmail.com>
2022-05-26 02:27:10 -0500

    ADD: solución kata10

--- src/main/java/katas/Kata10.java
+++ src/main/java/katas/Kata10.java
@@ -12,25 +12,15 @@ public class Kata10 {
     public static List<Map> execute() {
         List<Map> lists = DataUtil.getLists();
         List<Map> videos = DataUtil.getVideos();
-        return ImmutableList.of(ImmutableMap.of("name", "someName", "videos", ImmutableList.of(
-                ImmutableMap.of("id", 5, "title", "The Chamber"),
-                ImmutableMap.of("id", 3, "title", "Fracture")
-        )));
-    }
-
-    public static void main(String[] args) {
-        List<Map> lists = DataUtil.getLists();
-
-        List<Map> videos = DataUtil.getVideos();
-        var v = lists.stream().map(list -> ImmutableMap.of(
-                        "name", list.get("name"),
-                        "videos", ImmutableMap.of("id", videos.stream()
-                            .filter(video->video.get("listId").equals(list.get("id")))
-                            .map(video -> video.get("id"))
-                            .collect(Collectors.toList()))
-                        )).collect(Collectors.toList());
-        System.out.println(lists);
-        System.out.println(videos);
-        System.out.println(v);
+        return lists.stream().map(list -> ImmutableMap.of(
+                "name", list.get("name"),
+                "videos", videos.stream()
+                        .filter(video -> video.get("listId").equals(list.get("id")))
+                        .map(video -> ImmutableMap.of(
+                                "id", video.get("id"),
+                                "title", video.get("title")
+                        ))
+                        .collect(Collectors.toList())
+        )).collect(Collectors.toList());
     }
 }
