fe24613f8894aaf8813fb20d1d2a132756e67910
garciar316 <sergio980316@hotmail.com>
2022-05-26 03:16:12 -0500

    FIX: se corrige la kata10

--- src/main/java/katas/Kata10.java
+++ src/main/java/katas/Kata10.java
@@ -1,12 +1,50 @@
 package katas;
 
-import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import util.DataUtil;
 
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+
+/*
+    Goal: Create a datastructure from the given data:
+    We have 2 arrays each containing lists, and videos respectively.
+    Each video has a listId field indicating its parent list.
+    We want to build an array of list objects, each with a name and a videos array.
+    The videos array will contain the video's id and title.
+    In other words we want to build the following structure:
+    [
+        {
+            "name": "New Releases",
+            "videos": [
+                {
+                    "id": 65432445,
+                    "title": "The Chamber"
+                },
+                {
+                    "id": 675465,
+                    "title": "Fracture"
+                }
+            ]
+        },
+        {
+            "name": "Thrillers",
+            "videos": [
+                {
+                    "id": 70111470,
+                    "title": "Die Hard"
+                },
+                {
+                    "id": 654356453,
+                    "title": "Bad Boys"
+                }
+            ]
+        }
+    ]
+    DataSource: DataUtil.getLists(), DataUtil.getVideos()
+    Output: the given datastructure
+*/
 
 public class Kata10 {
     public static List<Map> execute() {
@@ -20,7 +58,7 @@ public class Kata10 {
                                 "id", video.get("id"),
                                 "title", video.get("title")
                         ))
-                        .collect(Collectors.toList())
-        )).collect(Collectors.toList());
+                        .collect(Collectors.toUnmodifiableList())
+        )).collect(Collectors.toUnmodifiableList());
     }
 }
